fb5823654716cd86b0515726f184ad091d391634
FaresElgharib <132018801+FaresElgharib@users.noreply.github.com>
2023-04-28 00:41:20 +0200

    Create Q2.java

---
+++ Q2.java
@@ -0,0 +1,34 @@
+package cw;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Q2 {
+
+	public static boolean sameSet(int[] arr1, int[] arr2) {
+	    // Create two hash sets to store the unique values in the arrays
+	    Set<Integer> set1 = new HashSet<>();
+	    Set<Integer> set2 = new HashSet<>();
+
+	    for (int i = 0; i < arr1.length; i++) {
+	        set1.add(arr1[i]);
+	    }
+
+	    for (int i = 0; i < arr2.length; i++) {
+	        set2.add(arr2[i]);
+	    }
+
+	    return set1.equals(set2);
+	}
+
+	public static void main(String[] args) {
+		int[] arr1 = {1, 2, 3, 4, 5};
+		int[] arr2 = {5, 4, 3, 2, 2};
+
+		if (sameSet(arr1, arr2)) {
+		    System.out.println("The arrays contain the same set of numbers.");
+		} else {
+		    System.out.println("The arrays do not contain the same set of numbers.");
+		}
+	}
+}
