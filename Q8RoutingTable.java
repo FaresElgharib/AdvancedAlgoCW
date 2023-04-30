7366174e485e7af7456bf5b669d1f7347a36fc75
FaresElgharib <132018801+FaresElgharib@users.noreply.github.com>
2023-04-30 04:36:00 +0200

    Add files via upload

---
+++ Q8RoutingTable.java
@@ -0,0 +1,54 @@
+package cw;
+
+import java.util.*;
+
+public class Q8RoutingTable {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of nodes in the network: ");
+        int numNodes = input.nextInt();
+        
+        Map<Character, List<Character>> connectivity = new HashMap<>();
+        for (int i = 0; i < numNodes; i++) {
+            System.out.print("Enter the connectivity information for node " + (char) ('A' + i) + ": ");
+            Scanner input1 = new Scanner(System.in);
+            String connectivityInfo = input1.nextLine();
+            System.out.println(connectivityInfo);
+            char node = connectivityInfo.charAt(5);
+            char neighbor = connectivityInfo.charAt(8);
+            if (!connectivity.containsKey(node)) {
+                connectivity.put(node, new ArrayList<>());
+            }
+            connectivity.get(node).add(neighbor);
+        }
+        
+        Map<Character, Map<Character, Character>> routingTables = new HashMap<>();
+        for (char node : connectivity.keySet()) {
+            Map<Character, Character> routingTable = new HashMap<>();
+            Queue<Character> queue = new LinkedList<>();
+            Set<Character> visited = new HashSet<>();
+            queue.offer(node);
+            visited.add(node);
+            while (!queue.isEmpty()) {
+                char curr = queue.poll();
+                for (char neighbor : connectivity.get(curr)) {
+                    if (!visited.contains(neighbor)) {
+                        visited.add(neighbor);
+                        queue.offer(neighbor);
+                        routingTable.put(neighbor, curr);
+                    }
+                }
+            }
+            routingTables.put(node, routingTable);
+        }
+        
+        for (char node : routingTables.keySet()) {
+            System.out.println("Routing table for node " + node + ":");
+            Map<Character, Character> routingTable = routingTables.get(node);
+            for (char dest : routingTable.keySet()) {
+                System.out.println("(" + dest + ", " + routingTable.get(dest) + ")");
+            }
+            System.out.println();
+        }
+    }
+}
