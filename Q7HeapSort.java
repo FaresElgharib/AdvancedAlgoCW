3d455b497546d488b43870d1c5e1e00daa6c9ae3
FaresElgharib <132018801+FaresElgharib@users.noreply.github.com>
2023-05-12 21:29:23 +0300

    Update Q7HeapSort.java

--- Q7HeapSort.java
+++ Q7HeapSort.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class Q7HeapSort {
   
   public static void main(String[] args) {
+    // Running the code
     int[] arr = {4, 1, 3, 9, 7, 55, 75};
     heapsort(arr);
     System.out.println(Arrays.toString(arr));
@@ -12,25 +13,31 @@ public class Q7HeapSort {
   
   public static void heapsort(int[] arr) {
     int n = arr.length;
-
+  
+    // Creating the min Heap
     for (int i = n/2 - 1; i >= 0; i--) {
       heapify(arr, n, i);
     }
 
+    // Getting the elements from the heap
     for (int i = n-1; i >= 0; i--) {
+      // Moving the minimum element to the end
       int temp = arr[0];
       arr[0] = arr[i];
       arr[i] = temp;
 
+      // Heapifying the remaining elements
       heapify(arr, i, 0);
     }
   }
 
   private static void heapify(int[] arr, int n, int i) {
-    int smallest = i;
-    int left = 2*i + 1;
-    int right = 2*i + 2;
+    
+    int smallest = i; // Root element
+    int left = 2*i + 1; // Left child element
+    int right = 2*i + 2; // Right child element
 
+    // Checking for the smallest element from root, left child, and right child
     if (left < n && arr[left] < arr[smallest]) {
       smallest = left;
     }
@@ -38,11 +45,14 @@ public class Q7HeapSort {
       smallest = right;
     }
 
+    // Checking if the "smallest" element is not the root
     if (smallest != i) {
+      // Swaping the "smallest" element with the root element
       int temp = arr[i];
       arr[i] = arr[smallest];
       arr[smallest] = temp;
 
+      // Heapifying the subtree recursively
       heapify(arr, n, smallest);
     }
   }
