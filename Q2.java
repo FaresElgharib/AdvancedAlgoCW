99b0c7c394a6152166ad977069106efc96f24c0c
FaresElgharib <132018801+FaresElgharib@users.noreply.github.com>
2023-05-12 21:44:23 +0300

    Update Q2.java

--- Q2.java
+++ Q2.java
@@ -6,9 +6,11 @@ import java.util.Set;
 public class Q2 {
 
 	public static boolean sameSet(int[] arr1, int[] arr2) {
+	    // Create 2 HashSets to store the unique elements
 	    Set<Integer> set1 = new HashSet<>();
 	    Set<Integer> set2 = new HashSet<>();
 
+	    // Adding the elements from the arrays to the sets
 	    for (int i = 0; i < arr1.length; i++) {
 	        set1.add(arr1[i]);
 	    }
@@ -17,10 +19,12 @@ public class Q2 {
 	        set2.add(arr2[i]);
 	    }
 
+	   // Checking if the sets are equal
 	    return set1.equals(set2);
 	}
 
 	public static void main(String[] args) {
+		// Running the code
 		int[] arr1 = {1, 2, 3, 4, 5};
 		int[] arr2 = {5, 4, 3, 2, 2};
 
