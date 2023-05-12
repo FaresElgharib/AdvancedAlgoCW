e65df45a87b3b3fc56cc8bce2e6c8e712f179285
FaresElgharib <132018801+FaresElgharib@users.noreply.github.com>
2023-05-12 21:42:01 +0300

    Update Q1.java

--- Q1.java
+++ Q1.java
@@ -9,17 +9,23 @@ import java.util.Set;
 public class Q1 {
 
 	public static char[] removeDuplicates(char[] input) {
+	    // Creating a HashSet to store the unique chars
 	    Set<Character> set = new HashSet<>();
+	    // Creating an array to storre the unique chars
 	    List<Character> result = new ArrayList<>();
 
+	    // Looping through the chars in the input array
 	    for (char c : input) {
+		// Checking if the char is already in the set
 	        if (!set.contains(c)) {
 	            set.add(c);
 	            result.add(c);
 	        }
 	    }
 
+	    // Creating a new char array with the right size
 	    char[] output = new char[result.size()];
+	    // Adding the chars from the result list to the array
 	    for (int i = 0; i < result.size(); i++) {
 	        output[i] = result.get(i);
 	    }
@@ -28,6 +34,7 @@ public class Q1 {
 	}
 	
 	public static void main(String[] args) {
+		// Running the code
 		char[] input = {'a', 'b', 'c', 'a', 'd', 'c', 'e'};
 		char[] result = removeDuplicates(input);
 		System.out.println(Arrays.toString(result));
